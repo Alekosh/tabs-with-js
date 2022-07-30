ac7dabe4b8bfd1955c1c385e4f23f31653f48268
Alekosh <aakhosh00@gmail.com>
2022-07-31 04:01:10 +0500

    fixed and used arrow method function

--- js/main.js
+++ js/main.js
@@ -1,24 +1,45 @@
 let tabs__header = document.querySelectorAll(".tab__header-item");
 let tab__body = document.querySelectorAll(".tab__body-item");
 
-tabs__header.forEach(function (tab, tab_index) {
-  tab.addEventListener("click", function () {
+// old
+// tabs__header.forEach(function (tab, tab_index) {
+//   tab.addEventListener("click", function () {
 
-    tabs__header.forEach(function (tab) {
-      tab.classList.remove("active"); //Сначала удаляем у всех класс active
+//     tabs__header.forEach(function (tab) {
+//       tab.classList.remove("active"); //Сначала удаляем у всех класс active
+//     });
+
+//     tab.classList.add("active"); //Затем добавяем класс active по нажатию
+
+//     tab__body.forEach(function (content, content_index) {
+//       if (content_index == tab_index) {
+//         content.style.display = "block";
+//       }
+//       else {
+//         content.style.display = "none";
+//       }
+//     });
+
+//   });
+
+// });
+
+// new ↓↓↓
+tabs__header.forEach((tab, index) => {
+
+  tab.addEventListener('click', () => {
+
+    tabs__header.forEach((tab) => {
+      tab.classList.remove('active');
     });
 
-    tab.classList.add("active"); //Затем добавяем класс active по нажатию
+    tabs__header[index].classList.add('active');
 
-    tab__body.forEach(function (content, content_index) {
-      if (content_index == tab_index) {
-        content.style.display = "block";
-      }
-      else {
-        content.style.display = "none";
-      }
+    tab__body.forEach((content) => {
+      content.classList.remove('is-active');
     });
 
+    tab__body[index].classList.add('is-active');
   });
 
 });
